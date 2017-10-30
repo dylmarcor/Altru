1f56fea1ffd5a4ec0d3f3a5dcaa79f05e098489c
Dylmarcor <dylancorbett@me.com>
2017-10-30 09:22:33 -0700

    Added login code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'sessions#new'
-  resources :users
+  resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
   get '/login', to: 'sessions#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029214330) do
+ActiveRecord::Schema.define(version: 20171030160901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20171029214330) do
     t.boolean "giver"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
+    t.string "password_confirmation"
   end
 
   add_foreign_key "chats", "users"

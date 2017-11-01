92f1b49e4838da0e687f142906a42ec962e56ff7
Dylmarcor <dylancorbett@me.com>
2017-11-01 15:49:19 -0700

    Edited user and chat pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'sessions#new'
   root to: 'pages#show', page: 'welcome'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   resources :chats, except: [:new] do
     resources :messages, only: [:create, :destroy], shallow: true
